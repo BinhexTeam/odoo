78d10c6a2832f5cf6ea1daee4a7720cee568b56a
Antonio David Ruban Espinal <antoniodavid8@gmail.com>
2025-07-21 16:54:44 -0400

    [MIG] goat: Coment res.country.state.csv

--- odoo/addons/base/__manifest__.py
+++ odoo/addons/base/__manifest__.py
@@ -26,7 +26,7 @@ The kernel of Odoo, needed for all installation.
         'views/base_menus.xml',
         'views/decimal_precision_views.xml',
         'views/res_config_views.xml',
-        'data/res.country.state.csv',
+        # 'data/res.country.state.csv',
         'views/ir_actions_views.xml',
         'views/ir_asset_views.xml',
         'views/ir_config_parameter_views.xml',
